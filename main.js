e33182eca49a1649b26168f6428976710267b54f
Melissa Gaines <gaines.melissa23@gmail.com>
2015-02-10 00:15:38 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,47 @@
+console.log("Why Hello There");
+alert("Why Hello There");
+//comment goes here
+//next array
+var RocketPower = ["Otto", "Twister"];
+console.log("RP");
+var Scooby = ["Scooby", "Shaggy"];
+console.log("Scoob");
+var shows = ["RocketPower", "Scooby"];
+var shows = [ ["Otto", "Twister"] , ["Scooby", "Shaggy"]];
+//adding arrays
+console.log(shows [0][0])
+console.log(shows[1][0])
+
+console.log("RP" + "Scoob")
+
+if(5<10){"right"} else {"wrong"}
+if(5>10){console.log("wrong");}
+if(5<10){console.log("right");}
+var mynumber = "5";
+if(mynumber<10){console.log("right");}
+if(mynumber>10){console.log("wrong");}
+function Number(five , ten){return five + 10 + ten}
+console.log(Number(5,10));
+
+function someName(firstNumber , secondNumber){return firstNumber + 11 + secondNumber;}
+console.log(someName(5,4));
+
+function right(){("Hey!");}
+console.log("right");
+
+var price= [2];
+console.log("price");
+var quantity = [15];
+console.log("quantity");
+function total(price , quantity){return price * quantity;}
+console.log(total(2,15));
+
+var width=[3];
+console.log("width");
+var height=[4];
+console.log("height");
+function area(width, height){return (width * height);};
+var size="area(3,4)";
+
+
+
